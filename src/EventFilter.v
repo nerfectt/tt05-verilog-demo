3b96ce14b7fbfdca2514ff34c16ab6c2dd4232f9
nerfectt <emlee0806@gmail.com>
2023-11-01 13:56:28 -0700

    more syntax errors

--- src/EventFilter.v
+++ src/EventFilter.v
@@ -1,20 +1,20 @@
 `default_nettype none
 
 module EventFilter (
-    input wire [15:0] x, // Assuming that x, y, & t are 16-bits
-    input wire [15:0] y,
-    input wire [15:0] t,
+    input wire [7:0] x, // Assuming that x, y, & t are 16-bits
+    input wire [7:0] y,
+    input wire [7:0] t,
     input wire p,        // Polarity is 1 bit (1 or 0 (or otherwise))
     input wire rst_n,
     input wire clk, // Clock input
 
-    output reg [15:0] x_out,
-    output reg [15:0] y_out,
-    output reg [15:0] t_out,
+    output reg [7:0] x_out,
+    output reg [7:0] y_out,
+    output reg [7:0] t_out,
     output reg p_out
 );
 
-wire [47:0] out;
+wire [24:0] out;
 
 
     always @(*) begin
@@ -28,27 +28,12 @@ wire [47:0] out;
     end
 
     always @(posedge clk) begin
-        x_out <= out[47:32]; // non-blocking
-        y_out <= out[31:16];
-        t_out <= out[15:0];
+        x_out <= out[24:16]; // non-blocking
+        y_out <= out[15:8];
+        t_out <= out[7:0];
         p_out <= p;
     end
 
 
 endmodule
-// always @* begin // Comb logic
-//     if (p == 1'b1) begin // If event is high, just output the tuplr
-//         x_out = x;
-//         y_out = y;
-//         p_out = p;
-//         t_out = t;
-//     end else begin
-//         // If the polarity is not +1, event is low -- is it better to output 0 or like X or Z?
-//         x_out = 16'b0; // can it be 16'bz as a dont care output?
-//         y_out = 16'b0;
-//         p_out = 1'b0;
-//         t_out = 16'b0;
-//     end
-// end
-
 
