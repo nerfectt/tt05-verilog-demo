1ce1cac39163ff5013ddd9360fefca5fd51af6f9
nerfectt <emlee0806@gmail.com>
2023-11-02 14:55:36 -0700

    more testing

--- src/test.py
+++ src/test.py
@@ -5,7 +5,10 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     my_high_Tuple = 151 # 10 01 01 11 
-    my_low_Tuple = 147 # 10 01 00 11
+    my_high_Tuple_new = 199 # 11 00 01 11
+    my_low_Tuple_1 = 147 # 10 01 00 11 thus p is 0
+    my_low_Tuple_2 = 159 # 10 01 11 11 thus p is 3
+    my_low_Tuple_3 = 155 # 10 01 10 11 thus p is 2
 
     dut._log.info("Start Sim")
 
@@ -18,18 +21,29 @@ async def test_my_design(dut):
     await ClockCycles(dut.clk,10)
     dut.rst_n.value = 1 # take out of reset, inverse high
 
+    # once out of reset Test1
     dut.ui_in.value = my_high_Tuple
     dut.ena.value = 1 # enable the design
 
-    # wait 10 cycles
+    # wait 10 cycles Test2
     await ClockCycles(dut.clk,10)
-    dut.ui_in.value = my_low_Tuple
+    dut.ui_in.value = my_low_Tuple_1
+
+    # wait 10 cycles Test3
+    await ClockCycles(dut.clk,10)
+    dut.ui_in.value = my_low_Tuple_2
+
+    # wait 10 cycles Test4
+    await ClockCycles(dut.clk,10)
+    dut.ui_in.value = my_low_Tuple_3
+
+    # back to high event and change the x,y,t tuple value
+    await ClockCycles(dut.clk,10)
+    dut.ui_in.value = my_high_Tuple_new
 
     # wait for a while and run for 100 cycles
     for _ in range(100):
         await RisingEdge(dut.clk)
-
-    #assert dut.ui_in.value == dut.uo_out.value
 
     dut._log.info("Finished Sim")
 
