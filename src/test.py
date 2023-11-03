a93dd811198e46497b828ba959e80c15ff2f460d
nerfectt <emlee0806@gmail.com>
2023-11-03 14:55:36 -0700

    fixed everyting

--- src/test.py
+++ src/test.py
@@ -5,7 +5,6 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     my_high_Tuple = 151 # 10 01 01 11 
-    my_high_Tuple_2 = 247 # 11 11 01 11
 
     my_low_Tuple = 147 # 10 01 00 11 thus p is 0
 
@@ -23,15 +22,15 @@ async def test_my_design(dut):
     dut.ena.value = 1 # enable the design
 
     # Test 1: 5 cycles of high P
-    dut.ui_in.value = my_high_Tuple # 151
-    await ClockCycles(dut.clk,5)
+    dut.ui_in.value = my_high_Tuple # 151 and should be output
+    await ClockCycles(dut.clk,5) 
 
     # wait for an extra clock cycle
     dut.ui_in.value = 0
     await ClockCycles(dut.clk,1)
 
     # Test 2: 5 cycles of low P
-    dut.ui_in.value = my_low_Tuple # 147
+    dut.ui_in.value = my_low_Tuple # 147 and should output
     await ClockCycles(dut.clk,6)
 
     # wait for an extra clock cycle
@@ -53,6 +52,27 @@ async def test_my_design(dut):
     await ClockCycles(dut.clk,4)
     dut.ui_in.value = my_high_Tuple # 151
     await ClockCycles(dut.clk,1)
+
+    # wait for an extra clock cycle
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk,1)
+
+    # Test 5: Random cycling 
+    dut.ui_in.value = my_low_Tuple # 147
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = my_high_Tuple # 151
+    await ClockCycles(dut.clk,2)
+    dut.ui_in.value = my_low_Tuple # 147
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = my_high_Tuple # 151 and should be output
+    await ClockCycles(dut.clk,6)
+
+    # wait for an extra clock cycle
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk,1)
+
+    dut.ui_in.value = my_low_Tuple # 147
+    await ClockCycles(dut.clk,2)
 
     # wait for an extra clock cycle
     dut.ui_in.value = 0
